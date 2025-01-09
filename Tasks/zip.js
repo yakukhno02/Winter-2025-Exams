fa6bff9d7d71daef834aaaa16860adf786dca6bc
ykmsh <mayak.ol.02.10@gmail.com>
2025-01-09 16:07:06 +0200

    refactor: use map in zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,25 +1,14 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 2
-// improve naming;
+// Step 3
+// use map to zip two arrays
 
 "use strict";
 
 const zip = function (array1 = [], array2 = []) {
-  let index1 = 0;
-  let index2 = 0;
-
-  for (let item of array2) {
-    const pair = [array1[index1++], item];
-    if (index1 < index2) {
-      delete array1[index1++];
-    } else {
-      array2[index2++] = pair;
-      if (pair[0] === undefined) array2.length -= 1;
-    }
-  }
-    return array2;
+  const minLength = Math.min(array1.length, array2.length);
+  return array1.slice(0, minLength).map((index1,index2) => [index1, array2[index2]]);
 };
 
 module.exports = zip;
