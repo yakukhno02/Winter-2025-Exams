6f7576bbe1f40da188cba02ef3c4126ac1711358
ykmsh <mayak.ol.02.10@gmail.com>
2025-01-09 16:13:30 +0200

    fix: avoid mutation of incoming parameters in zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,14 +1,16 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 3
-// use map to zip two arrays
+// Step 4
+// don't mutate parameters
 
 "use strict";
 
 const zip = function (array1 = [], array2 = []) {
   const minLength = Math.min(array1.length, array2.length);
-  return array1.slice(0, minLength).map((index1,index2) => [index1, array2[index2]]);
+  const newArray1 = [...array1];
+  const newArray2 = [...array2];
+  return newArray1.slice(0, minLength).map((index1,index2) => [index1, newArray2[index2]]);
 };
 
 module.exports = zip;
