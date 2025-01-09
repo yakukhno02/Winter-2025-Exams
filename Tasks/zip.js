a02459a512a8abd2e99d54c034e5af8200f3521b
ykmsh <mayak.ol.02.10@gmail.com>
2025-01-09 15:31:00 +0200

    refactor: add 'use strict' and let/const zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,19 +1,27 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
+// Step 1
+// add "use strict";
+// add 'const' and 'let';
+// remove senseless block;
+
+"use strict";
+
 const zip = function (a = [], b = []) {
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
+  let j = 0;
+
+  for (let x of b) {
+    const CELL = [a[i++], x];
     if (i < j) {
       delete a[i++];
     } else {
-      (() => (b[j++] = CELL))();
+      b[j++] = CELL;
+      if (CELL[0] === undefined) b.length -= 1;
     }
-    if (CELL[0] == undefined) b.length -= 1;
   }
-  return b;
+    return b;
 };
 
 module.exports = zip;
