878ae0be49ede78ff51f9f4ba7f203af1bf41556
ykmsh <mayak.ol.02.10@gmail.com>
2025-01-09 14:23:04 +0200

    refactor: remove senseless blocks in between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,10 +1,9 @@
 // Refactor following solution
 // Extract substring between prefix and suffix
 
-// Step 1
-// add 'use strict';
-// Improve naming;
-// Add const and let;
+// Step 2
+// remove senseless blocks;
+// Add spacing;
 
 
 "use strict";
@@ -12,18 +11,17 @@
 const getValueBetween = (string, prefix, suffix) => {
   let index = string.indexOf(prefix);
   if (index === -1) return "";
-  else {
-    const key = index + prefix.length;
-    string = string.substring(key);
-    if (suffix) {
-      index = string.indexOf(suffix);
-      if (index === -1) {
-        return "";
-      } else {
-        string = string.substring(0, index);
-      }
+
+  const key = index + prefix.length;
+  string = string.substring(key);
+
+  if (suffix) {
+    index = string.indexOf(suffix);
+    if (index === -1) return "";
+
+    string = string.substring(0, index);
     }
-  }
+
   return string;
 };
 
