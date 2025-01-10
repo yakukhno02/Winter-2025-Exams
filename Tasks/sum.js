64645053c07e3ed0e429e59b40596ec8d250798d
ykmsh <mayak.ol.02.10@gmail.com>
2025-01-10 12:51:29 +0200

    refactor: use reduce for summing in sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,19 +1,11 @@
 // Refactor following solution
 // Sum all numbers from an array
 
-// Step 3
-// Remove unnecessary checks to simplify logic
+// Step 4
+// Replace loop (for ... of ...) with reduce method;
 
 "use strict";
 
-const calcSum = (array) => {
-  let total = 0
-  for (const item of array) {
-    if (typeof item === "number") {
-        total += item;
-    }
-  }
-  return total;
-};
+const calcSum = (array) => array.reduce((total, item) => (typeof item === "number"? total + item : total), 0);
 
 module.exports = calcSum;
